7a4ef69350ae9b82cf3736ce2a407cd26569594e
gyorik <47569981+gyorik@users.noreply.github.com>
2019-03-27 11:47:58 +0100

    asd

--- skeleton in inteij/src/com/company/Breakable.java
+++ skeleton in inteij/src/com/company/Breakable.java
@@ -1,14 +1,14 @@
 package com.company;
 
 public class Breakable extends Field {
-	private int remainLifetime;
+	private int remainLifetime;//Hátralévő élet
 
 
-	public void stepped(Unit u) {
+	public void stepped(Unit u) {//Ha rálépnek meghívódik Unit u:aki rá lépett
 		if (u!= null)
 		{
 			remainLifetime--;
-			if (remainLifetime<1)
+			if (remainLifetime<1)//Ha eltörik, aki rajta áll meghal
 			{
 				u.die();
 			}

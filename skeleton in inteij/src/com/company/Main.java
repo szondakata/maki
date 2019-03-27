e9af74f573e452a105bbd7c7bcd393ef29ec5463
gyorik <47569981+gyorik@users.noreply.github.com>
2019-03-27 14:23:28 +0100

    keki

--- skeleton in inteij/src/com/company/Main.java
+++ skeleton in inteij/src/com/company/Main.java
@@ -63,10 +63,98 @@ public class Main {
 
     }
 
-    public static void movepanda_test(){}
-    static public void moveorangutan_test(){}
-    static public void release_test(){}
-    static public void end_test(){}
+    public static void movepanda_test(){
+        ArrayList<Field> fields = new ArrayList<>();
+        fields.add(new Field());
+        fields.add(new Field());
+        fields.add(new Field());
+        fields.add(new Exit());
+        Panda p1 = new Panda();
+        Panda p2 = new Panda();
+        p2.setIamon(fields.get(0));
+        fields.get(0).setContain(p2);
+        p1.setIamon(fields.get(1));
+        fields.get(1).setContain(p1);
+        p2.setHand1(p1);
+        p1.setHand2(p2);
+        p1.move(fields.get(2));
+        p1.move(fields.get(3));
+        System.out.println("Move panda test done!");
+    }
+
+    static public void moveorangutan_test(){
+        Control control = new Control();
+        ArrayList<Field> fields = new ArrayList<>();
+        fields.add(new Field());
+        fields.add(new Field());
+        fields.add(new Field());
+        fields.add(new Field());
+        fields.add(new Field());
+        Panda p1 = new Panda();
+        Panda p2 = new Panda();
+        control.pandas.add(p1);
+        control.pandas.add(p2);
+        Orangutan orangutan = new Orangutan();
+        Orangutan enemy = new Orangutan();
+        control.orangutans.add(orangutan);
+        control.orangutans.add(enemy);
+        Player player1 = new Player();
+        Player player2 = new Player();
+        control.setP1(player1);
+        control.setP2(player2);
+        p1.setIamon(fields.get(0));
+        fields.get(0).setContain(p1);
+        orangutan.setIamon(fields.get(1));
+        fields.get(1).setContain(orangutan);
+        p2.setIamon(fields.get(3));
+        fields.get(3).setContain(p2);
+        enemy.setIamon(fields.get(4));
+        fields.get(4).setContain(enemy);
+        orangutan.setHand2(p1);
+        p1.setHand1(orangutan);
+        control.move(control.orangutans.get(0),fields.get(2));
+        control.move(control.orangutans.get(0),fields.get(3));
+        control.move(control.orangutans.get(0),fields.get(4));
+        System.out.println("Move Orangutan test done!");
+    }
+
+    static public void release_test(){
+        ArrayList<Field> fields = new ArrayList<>();
+        fields.add(new Field());
+        fields.add(new Field());
+        fields.add(new Field());
+        fields.add(new Field());
+        fields.add(new Field());
+        fields.add(new Field());
+        Panda p1 = new Panda();
+        Panda p2 = new Panda();
+        Panda p3 = new Panda();
+        Panda p4 = new Panda();
+        Orangutan orangutan = new Orangutan();
+        fields.get(0).setContain(p1);
+        p1.setIamon(fields.get(0));
+        fields.get(1).setContain(p2);
+        p2.setIamon(fields.get(1));
+        fields.get(2).setContain(p3);
+        p3.setIamon(fields.get(2));
+        fields.get(3).setContain(p4);
+        p4.setIamon(fields.get(3));
+        fields.get(4).setContain(orangutan);
+        orangutan.setIamon(fields.get(4));
+        orangutan.move(fields.get(3));
+        orangutan.move(fields.get(2));
+        orangutan.move(fields.get(1));
+        orangutan.move(fields.get(0));
+        p2.release();
+        System.out.println("Realease test done!");
+    }
+
+
+    static public void end_test(){
+
+    }
+
+
     static public void usewardrobe_test(){}
     static public void orangutanexit_test(){}
     static public void pandadies_test(){}

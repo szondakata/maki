c4675f4262e37bd936dc7d7fb616a94053272269
szondakata <katalin.szonda@gmail.com>
2019-03-27 15:54:06 +0100

    javítva

--- skeleton in inteij/src/com/company/Control.java
+++ skeleton in inteij/src/com/company/Control.java
@@ -48,7 +48,7 @@ public class Control {
     }
 
     public boolean End() {//TODO
-        if (orangutans.get(0).isAliive && orangutans.get(1).isAliive && pandas.size() < 1) {  //ha egy orángután meghal vagy elfogynak a pandák vége a játéknak
+        if (orangutans.get(0).isAlive && orangutans.get(1).isAlive && pandas.size() < 1) {  //ha egy orángután meghal vagy elfogynak a pandák vége a játéknak
             System.out.println("Jatekos 1 pontszam: " + p1.getPoints() + "\nJatekos 2 pontszam: " + p2.getPoints());
             return true;
         }

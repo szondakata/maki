1f8ecd350fecb095c05aa8347f6d257702d8a013
Vgem2 <tataititi2@gmail.com>
2019-03-27 17:16:25 +0100

    Update_Unit_w_Logger

--- skeleton in inteij/src/com/company/Unit.java
+++ skeleton in inteij/src/com/company/Unit.java
@@ -4,39 +4,75 @@ package com.company;
 public abstract class Unit implements Action {
 
 	boolean isAlive = true;
+	protected Logger logger;
 	private Field iamon;//Melyik mezőn vagyok
 	private Unit hand1;//"Hátra" mutató kéz
 	private Unit hand2;//"Előre" mutató kéz
 
+public Unit()
+	{
+		logger=new Logger();
+	}
 
 	public Field getIamon() {
+		logger.depthP();
+		logger.writeMessage(this.toString()+".getIamon()");
+		logger.depthM();
 		return iamon;
+
 	}
 	public void setIamon(Field iamon) {
+		logger.depthP();
+		logger.writeMessage(this.toString()+".setIamon("+iamon.toString()+")");
+		logger.depthM();
 		this.iamon = iamon;
 	}
 
 	public Unit getHand1() {
+
+		logger.depthP();
+		logger.writeMessage(this.toString()+".getHand1()");
+		logger.depthM();
 		return hand1;
 	}
 	public void setHand1(Unit hand1) {
+
+		logger.depthP();
+		logger.writeMessage(this.toString()+".setHand1("+hand1+")");
+		logger.depthM();
 		this.hand1 = hand1;
 	}
 
 	public Unit getHand2() {
+		logger.depthP();
+		logger.writeMessage(this.toString()+".getHand2()");
+		logger.depthM();
 		return hand2;
 	}
 	public void setHand2(Unit hand2) {
+		logger.depthP();
+		logger.writeMessage(this.toString()+".setHand2("+hand2+")");
+		logger.depthM();
 		this.hand2 = hand2;
 	}
 
 	
-	public void grab(Unit u) {} //Üres osztály
+	public void grab(Unit u) {
+		logger.depthP();
+		logger.writeMessage(this.toString()+".grab("+u.toString()+")");
+		logger.depthM();
+	} //Üres osztály
 	
-	public void exit(Field f) {}
+	public void exit() {
+		logger.depthP();
+		logger.writeMessage(this.toString()+".exit()");
+		logger.depthM();
+	}
 	
 	public void die() {
-
+		logger.depthP();
+		logger.writeMessage(this.toString()+".die()");
+		logger.depthM();
 		this.setAlive(false);
 
 	}
@@ -45,6 +81,8 @@ public abstract class Unit implements Action {
 	//Mozgást valósít meg false-al tér vissza ha olyan mezőre próbál lépni ami foglalt
 	public boolean move(Field f)
 	{
+		logger.depthP();
+		logger.writeMessage(this.toString()+".move("+f.toString()+")");
 		if (f.getContain()==null)//Léphetek-e oda?
 		{
 			f.setContain(this);
@@ -55,13 +93,18 @@ public abstract class Unit implements Action {
 				this.getHand1().move(temp);
 			}
 			this.setIamon(f);
+			logger.depthM();
+
 			return true;
 		}
-		else{return  false;}
+		else{logger.depthM();
+			return  false;}
 	}
 
 	//A sor felbomlásáért felelős osztály
 	public void release() {
+		logger.depthP();
+		logger.writeMessage(this.toString()+".release()");
         if(getHand1() != null)
         {
             getHand1().setHand2(null);
@@ -71,13 +114,20 @@ public abstract class Unit implements Action {
         {
             getHand2().release();
         }
+        logger.depthM();
 	}
 
 	public boolean isAlive() {
+		logger.depthP();
+		logger.writeMessage(this.toString()+".isAlive()"+")");
+		logger.depthM();
 		return isAlive;
 	}
 
 	public void setAlive(boolean alive) {
+		logger.depthP();
+		logger.writeMessage(this.toString()+"setAlive("+alive+")");
+		logger.depthM();
 		isAlive = alive;
 	}
 }

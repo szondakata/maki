a576ff78bdcb81ce7d30b7ca58e22f63fd0d97e3
szondakata <katalin.szonda@gmail.com>
2019-05-15 06:18:57 +0200

    logger commented

--- maki-grafichs/src/com/company/Logger.java
+++ maki-grafichs/src/com/company/Logger.java
@@ -7,40 +7,51 @@ package com.company;
 public class Logger {
 
     /**
-     *
+     * Indentálásért felelős változó
      */
     private static int depth = -1;
 
+
+    /**
+     * A jelenlegi indentálás mélységét adja vissza
+     *
+     * @return indentálás jelenlegi mélysége
+     */
     public static int getDepth() {
         return depth;
     }
 
+    /**
+     * A jelenlegi indentálás mélységét állítja be
+     *
+     * @param dep jelenlegi indentálás mélységét állítja be
+     */
     public static void setDepth(int dep) {
         depth = dep;
     }
 
     /**
-     *
+     * A jelenlegi indentálás mélységét növeli
      */
-    public static void depthP()
-    {
+    public static void depthP() {
         depth++;
     }
 
     /**
-     *
+     * A jelenlegi indentálás mélységét csökkenti
      */
-    public static void depthM()
-    {
+    public static void depthM() {
         depth--;
     }
 
     /**
-     * @param message
+     * A paraméterben kapott üzenetet jeleníti meg a std outputon
+     *
+     * @param message a kiírt üzenet
      */
     public static void writeMessage(String message) {
-       for(int i =0; i<depth;i++)
-           System.out.print("\t");
+        for (int i = 0; i < depth; i++)
+            System.out.print("\t");
         System.out.println(message);
     }
 }
